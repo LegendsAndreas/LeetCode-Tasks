1e89168d26218b1b6a42fe08aeb4b7b5b379c93e
Andreas Christoffersen <aoch21@student.aau.dk>
2024-03-14 20:08:49 +0100

    Add duplicate number detection functions
    This commit introduces two new functions, findDuplicate1 and findDuplicate2, designed to detect the presence of duplicate numbers within an array. The first function accomplishes this by sorting the array and comparing adjacent elements. In contrast, the second function navigates through each element, comparing it with the remaining elements from the following index.

---
+++ LeetCode287/dublicateNumber.go
@@ -0,0 +1,59 @@
+// https://leetcode.com/problems/find-the-duplicate-number/
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var numbers = [5]int{8, 2, 1, 9, 9}
+	fmt.Println("The duplicate number is:", findDuplicate1(numbers))
+	fmt.Println("The duplicate number is:", findDuplicate2(numbers))
+}
+
+// findDuplicate1 takes in a slice of integers 'nums' and returns the duplicate number found in the slice.
+// The function first sorts the slice in ascending order using the sort.Ints() function from the 'sort' package.
+// It then iterates through the sorted slice starting from index 1, comparing each element with the previous element.
+// If a duplicate number is found, it is assigned to the variable 'duplicateNumber', and the loop is exited.
+// Finally, the function returns the 'duplicateNumber' found.
+func findDuplicate1(nums [5]int) int {
+	var duplicateNumber int
+
+	sort.Ints(nums[:]) // Since the sort.Ints() has to use a slice, we insert a semicolon(:).
+
+	for i := 1; i < len(nums); i++ { // We start at 1, and then compare with the element before it, to avoid comparing outside the array.
+		if nums[i] == nums[i-1] {
+			duplicateNumber = nums[i]
+			break
+		}
+	}
+
+	return duplicateNumber
+}
+
+// findDuplicate2 takes in a fixed-size array of integers 'nums' and returns the duplicate number found in the array.
+// It iterates through each element of the array and compares it with the remaining elements starting from the next index.
+// If a duplicate number is found, it is assigned to the variable 'duplicateNumber' and returned immediately.
+// If no duplicate is found, the function returns 0.
+// Note that findDuplicate2 does not sort the array before finding the duplicate, unlike findDuplicate1.
+func findDuplicate2(nums [5]int) int {
+	var duplicateNumber int
+
+	for i := range nums {
+
+		for j := i + 1; j < len(nums); j++ { // To make sure we dont compare with the same index, we add 1 to j.
+			if nums[i] == nums[j] {
+				duplicateNumber = nums[i]
+				return duplicateNumber
+			}
+		}
+	}
+
+	return duplicateNumber
+}
+
+/* ToDo: make this work with a stack instead.
+func findDuplicate3(nums [5]int) int {
+
+}*/
