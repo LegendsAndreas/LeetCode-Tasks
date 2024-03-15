693753c0fd722d50415663d834a55a13b6ae54b9
Andreas <aoch21@student.aau.dk>
2024-03-15 16:37:36 +0100

    Update function signatures and array definitions
    The function signatures for findDuplicate1 and findDuplicate2 were updated to take in slices of integers instead of fixed size arrays. Additionally, the variable numbers was refactored to a slice and a new slice numbers2 was introduced for cleaner code usage.

--- LeetCode287/dublicateNumber.go
+++ LeetCode287/dublicateNumber.go
@@ -7,9 +7,10 @@ import (
 )
 
 func main() {
-	var numbers = [5]int{8, 2, 1, 9, 9}
+	var numbers = []int{8, 2, 1, 9, 9}
+	var numbers2 = []int{8, 2, 1, 9, 9}
 	fmt.Println("The duplicate number is:", findDuplicate1(numbers))
-	fmt.Println("The duplicate number is:", findDuplicate2(numbers))
+	fmt.Println("The duplicate number is:", findDuplicate2(numbers2))
 }
 
 // findDuplicate1 takes in a slice of integers 'nums' and returns the duplicate number found in the slice.
@@ -17,10 +18,10 @@ func main() {
 // It then iterates through the sorted slice starting from index 1, comparing each element with the previous element.
 // If a duplicate number is found, it is assigned to the variable 'duplicateNumber', and the loop is exited.
 // Finally, the function returns the 'duplicateNumber' found.
-func findDuplicate1(nums [5]int) int {
+func findDuplicate1(nums []int) int {
 	var duplicateNumber int
 
-	sort.Ints(nums[:]) // Since the sort.Ints() has to use a slice, we insert a semicolon(:).
+	sort.Ints(nums) // Since the sort.Ints() has to use a slice, we insert a semicolon(:).
 
 	for i := 1; i < len(nums); i++ { // We start at 1, and then compare with the element before it, to avoid comparing outside the array.
 		if nums[i] == nums[i-1] {
@@ -37,7 +38,7 @@ func findDuplicate1(nums [5]int) int {
 // If a duplicate number is found, it is assigned to the variable 'duplicateNumber' and returned immediately.
 // If no duplicate is found, the function returns 0.
 // Note that findDuplicate2 does not sort the array before finding the duplicate, unlike findDuplicate1.
-func findDuplicate2(nums [5]int) int {
+func findDuplicate2(nums []int) int {
 	var duplicateNumber int
 
 	for i := range nums {
